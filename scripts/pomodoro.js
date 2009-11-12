b439ea5782798d959d17ce61052ab880408386ea
Stephan Meissner <atitsbest@gmail.com>
2009-11-12 15:31:14 +0100

    Init.

---
+++ scripts/pomodoro.js
@@ -0,0 +1,144 @@
+/**
+ @class: PomodoroController
+ */
+var PomodoroController = function() {
+  // Members:
+  this.POMODORO_DURATION_IN_MINUTES = 1;
+  this.remainingSeconds = this.POMODORO_DURATION_IN_MINUTES * 60;
+  // Liste der fertigen Pomodori
+  this.pomodoriCount = 1;
+
+  // Pausen und Pomodori.
+  this.timerDurationsInMinutes = [25, 5, 25, 5, 25, 5, 25, 15];
+  this.currentTimer = 0;
+
+  // Timer:
+  this.timerHandle = null;
+
+  // Elemente:
+  this.$timer = $('#timer');
+  this.$minutes = this.$timer.find('.minutes');
+  this.$seconds = this.$timer.find('.seconds');
+  this.$controls = $('#controls');
+  this.$startPomodoro = this.$controls.find('.start');
+  this.$cancelPomodoro = this.$controls.find('.cancel');
+  this.$pomodori = $('#pomodori > li');
+
+  this.init();
+}
+
+PomodoroController.prototype = {
+  /**
+   * CTR
+   */
+  init: function() {
+    var that = this;
+
+    // Events registrieren:
+    this.$startPomodoro.click(function() { that.onStartPomodoro(); });
+    this.$cancelPomodoro.click(function() { that.onCancelPomodoro(); });
+  },
+
+  /**
+   * Der Pomodoro wird gestartet.
+   */
+  onStartPomodoro: function() {
+    var that = this;
+
+    // Zeit auf die volle Dauer setzen.
+    this.remainingSeconds = this.timerDurationsInMinutes[this.currentTimer] * 60;
+
+    // Interval installieren.
+    this.timerHandle = window.setInterval(function() {
+      that.onTimerIntervall();
+    },
+    1000);
+
+    // Controls wechseln:
+    this.$startPomodoro.hide();
+    this.$cancelPomodoro.show();
+
+    this._displayPomodori();
+  },
+
+  /**
+   * Pomodoro wird abgebrochen (zählt also nicht).
+   */
+  onCancelPomodoro: function() {
+    if (this.timerHandle) {
+      window.clearInterval(this.timerHandle);
+    }
+
+    // Controls in Ausgangsposition.
+    this.$startPomodoro.show();
+    this.$cancelPomodoro.hide();
+  },
+
+  /**
+   * Wird jede Sekunde aufgerufen.
+   */
+  onTimerIntervall: function() {
+    this.remainingSeconds -= 1;
+    this.$minutes.html(this._formatLeadingZero(parseInt(this.remainingSeconds / 60)));
+    this.$seconds.html(this._formatLeadingZero(this.remainingSeconds % 60));
+
+    // Der Pomodoro ist fertig.
+    if (this.remainingSeconds == 0) {
+      this._pomodoroFinished();
+    }
+  },
+
+  /**
+   * Der aktuelle Pomodoro wurde gerade beendet.
+   */
+  _pomodoroFinished: function() {
+
+    this.onCancelPomodoro();
+
+
+    // Anzahl der fertigen Pomodori ermitteln.
+    if ((this.currentTimer % 2) == 0) {
+      setTimeout(function() { $.sound.play('sounds/ringin.wav'); }, 10);
+      this.pomodoriCount += 1;
+      // Nie mehr als 4 Pomodori:
+      if (this.pomodoriCount > 4)
+        this.pomodoriCount = 1;
+    }
+
+    // Nächster Durchlauf.
+    this.currentTimer += 1;
+    if (this.currentTimer >= this.timerDurationsInMinutes.length)
+      this.currentTimer = 0;
+
+    this.onStartPomodoro();
+  },
+
+  /**
+   * Zeigt die fertigen und den aktuellen Pomodoro an.
+   */
+  _displayPomodori: function() {
+    var that = this;
+
+    // Anzahl der fertigen Pomodori anzeigen.
+    this.$pomodori.each(function(index, li) {
+      var $li = $(li);
+      if (index < that.pomodoriCount && !$li.is(':visible')) {
+        $li.show('fast');
+      }
+      else if (index >= that.pomodoriCount) {
+        $li.hide();
+      }
+    });
+  },
+
+  /**
+   * Hilfsfunktion: Fügt eine 0 bei einstelligen Zahlen an.
+   */
+  _formatLeadingZero: function(number) {
+    return number < 10
+      ? "0" + number
+      : number.toString();
+  }
+};
+
+var app = new PomodoroController();
---
+++ scripts/system/string.js
@@ -0,0 +1,58 @@
+﻿/**
+ * Erweiterungen zum String-Objekt.
+ */
+if (!(new String).trim) {
+	String.prototype.trim = function() { return this.replace(/^\s+|\s+$/g,''); };
+}
+if (!(new String).normalize && (new String).trim) {
+	String.prototype.normalize = String.prototype.normalise = function() { return this.trim().replace(/\s+/g,' '); };
+}
+if (!(new String).startsWith) {
+	String.prototype.startsWith = function(str,i){ i=(i)?'i':'';var re=new RegExp('^'+str,i);return (this.normalize().match(re)) ? true : false ; };
+}
+if (!(new String).endsWith) {
+	String.prototype.endsWith = function(str,i){ i=(i)?'gi':'g';var re=new RegExp(str+'$',i);return (this.normalize().match(re)) ? true : false ; };
+}
+
+/**
+ * Zufallsstring
+ */
+String.prototype.rand = function(length) {
+	var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+	var randomstring = '';
+	for (var i=0; i<length; i++) {
+		var rnum = Math.floor(Math.random() * chars.length);
+		randomstring += chars.substring(rnum,rnum+1);
+	}
+	return randomstring;
+}
+
+/**
+ * Format z.b.: var msg = 'Mein Name ist {0}.'.format('Stephan');
+ */
+String.prototype.format = function() {
+  var str = this;
+
+  for(var i=0;i<arguments.length;i++) {
+    var re = new RegExp('\\{' + (i) + '\\}','gm');
+    str = str.replace(re, arguments[i]);
+  }
+
+  return str;
+}
+
+/**
+ * Erstellt ein eine GUID.
+ */
+String.generateGuid = function() {
+  var result, i, j;
+  result = '';
+  for(j=0; j<32; j++) {
+    if( j == 8 || j == 12|| j == 16|| j == 20)
+      result = result + '-';
+    
+    i = Math.floor(Math.random()*16).toString(16).toUpperCase();
+    result = result + i;
+  }
+  return result;
+} 
